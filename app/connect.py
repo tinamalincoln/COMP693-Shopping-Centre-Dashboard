906f666a455111f550deef65a25a20168dc2f9ae
tinamalincoln <Tina.Ma@lincolnuni.ac.nz>
2025-07-28 23:06:13 +1200

    Update connect.py

--- app/connect.py
+++ app/connect.py
@@ -5,4 +5,4 @@ dbuser = "root" #PUT YOUR MySQL username here - usually root
 dbpass = "Ywukio921RHH0o4Ak6wW" #PUT YOUR PASSWORD HERE
 dbhost = "localhost" 
 dbport = "3306"
-dbname = "shoppingmap"
+dbname = "shoppingmall"
