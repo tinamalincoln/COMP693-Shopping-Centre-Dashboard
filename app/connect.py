839607cb89d23f7558456f7d630ea6fa78677eb9
tinamalincoln <Tina.Ma@lincolnuni.ac.nz>
2025-08-08 21:37:27 +1200

    Remove files now in .gitignore

--- app/connect.py
+++
@@ -1,8 +0,0 @@
-# These details are available on the first MySQL Workbench screen
-# Usually called 'Local Instance'
-
-dbuser = "shoppingcentre" #PUT YOUR MySQL username here - usually root
-dbpass = "Ywukio921RHH0o4Ak6wW" #PUT YOUR PASSWORD HERE
-dbhost = "shoppingcentre.mysql.pythonanywhere-services.com" 
-dbport = "3306"
-dbname = "shoppingcentre$shoppingcentre"
